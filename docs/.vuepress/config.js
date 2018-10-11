32f5b50ac566b57a076ac04d51fa34e9e561f9fc
q5276 <q5276@126.com>
2018-10-11 10:25:02 +0800

    change something

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -53,7 +53,7 @@ module.exports = {
 
     //帮助编辑
     // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-    repo: 'vuejs/vuepress',
+    repo: 'q5276/vuestudy',
     // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
     // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
     repoLabel: '查看源码',
@@ -61,7 +61,7 @@ module.exports = {
     // 以下为可选的编辑链接选项
 
     // 假如你的文档仓库和项目本身不在一个仓库：
-    docsRepo: 'vuejs/vuepress',
+    docsRepo: 'q5276/vuestudy',
     // 假如文档不是放在仓库的根目录下：
     docsDir: 'docs',
     // 假如文档放在一个特定的分支下：
